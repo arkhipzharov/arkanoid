e2731b699a813ad1ec67730745225a7f34f3618e
Arkhip Zharov <arkhipzharov627@gmail.com>
2020-07-25 00:38:53 +0300

    mouse move racket, counters, reset class mixin
    also:
    * lose game support

--- js/index.js
+++ js/index.js
@@ -1,12 +1,28 @@
 import numToPx from './helpers/num-to-px.js';
 import delay from './helpers/delay.js';
 import randomBetween from './helpers/random-between.js';
+import capitalizeFirstLetter from './helpers/capitalize-first-letter.js';
 import reset from './mixins/reset.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const game = new Game();
-  game.setGameAreaChildBasePositions();
+  const RACKET_TOP = 250;
+  const ballEl = document.querySelector('.ball');
+  game.setGameAreaChildBasePositions(
+    [
+      {
+        className: 'racket',
+        top: RACKET_TOP,
+      },
+      {
+        className: 'ball',
+        top: RACKET_TOP - ballEl.offsetHeight,
+      },
+    ],
+    true,
+  );
   game.fillGameAreaWithBricks();
+  game.setPlayerStatsAndGameInfo();
   showHowToPlayInstructionOnce();
 });
 
@@ -16,109 +32,115 @@ function showHowToPlayInstructionOnce() {
   localStorage.isHowToPlayInstructionShown = true;
 }
 
-document.addEventListener('keydown', async (e) => {
-  const keyCode = e.code;
-  switch (keyCode) {
-    case 'ArrowLeft':
-    case 'ArrowRight':
-      const racketMoving = new RacketMoving(keyCode);
-      racketMoving.move();
-      break;
-    case 'Enter':
-      const game = new Game();
-      await game.startGame();
-      break;
-  }
-});
+document.addEventListener(
+  'mousemove',
+  (e) => {
+    const racketMoving = new RacketMoving();
+    racketMoving.moveRacketWhenMouseOverGameArea(e);
+  },
+  { passive: true }
+);
 
 class RacketMoving {
-  constructor(keyCode) {
-    this.keyCode = keyCode;
-  }
-  move() {
-    const RACKET_X_MOVE_STEP = 60;
+  static currRacketLeftRelativeToGameArea = null;
+  moveRacketWhenMouseOverGameArea(e) {
     const gameAreaRect = document
       .querySelector('.game-area__inner')
       .getBoundingClientRect();
     const racket = document.querySelector(`.racket`);
-    const racketRect = racket.getBoundingClientRect();
-    const racketLeft = racketRect.left - gameAreaRect.left;
-    const racketStyle = racket.style;
-    const keyCode = this.keyCode;
-    switch (keyCode) {
-      case 'ArrowLeft':
-        if (racketLeft - RACKET_X_MOVE_STEP < 0) {
-          racketStyle.left = numToPx(0);
-        } else {
-          racketStyle.left = numToPx(racketLeft - RACKET_X_MOVE_STEP);
-        }
-        break;
-      case 'ArrowRight':
-        if (racketRect.right + RACKET_X_MOVE_STEP > gameAreaRect.right) {
-          racketStyle.left = numToPx(gameAreaRect.width - racketRect.width);
-        } else {
-          racketStyle.left = numToPx(racketLeft + RACKET_X_MOVE_STEP);
-        }
-        break;
-    }
+    this.setElPositionBasedOnMouseMoveOverGameArea(gameAreaRect, e, racket);
     if (Game.isPlaying) return;
     const ballEl = document.querySelector(`.ball`);
-    const ballRect = ballEl.getBoundingClientRect();;
-    const ballLeft = ballRect.left - gameAreaRect.left;
-    const ballStyle = ballEl.style;
-    switch (keyCode) {
-      case 'ArrowLeft':
-        if (racketLeft - RACKET_X_MOVE_STEP < 0) {
-          ballStyle.left = numToPx((racketRect.width - ballRect.width) / 2);
-        } else {
-          ballStyle.left = numToPx(ballLeft - RACKET_X_MOVE_STEP);
-        }
-        break;
-      case 'ArrowRight':
-        if (racketRect.right + RACKET_X_MOVE_STEP > gameAreaRect.right) {
-          ballStyle.left = numToPx(gameAreaRect.width - ((racketRect.width - ballRect.width) / 2 + ballRect.width));
-        } else {
-          ballStyle.left = numToPx(ballLeft + RACKET_X_MOVE_STEP);
-        }
-        break;
+    this.setElPositionBasedOnMouseMoveOverGameArea(gameAreaRect, e, ballEl, racket);
+  }
+  setElPositionBasedOnMouseMoveOverGameArea(
+    gameAreaRect,
+    mouseMoveEvent,
+    el,
+    elToCenterPrevElRelativeTo,
+  ) {
+    const elRect = el.getBoundingClientRect();
+    const elWidth = elRect.width;
+    const elWidthHalf = elWidth / 2;;
+    const elStyle = el.style;
+    const { left: gameAreaLeft, width: gameAreaWidth } = gameAreaRect;
+    const clientX = mouseMoveEvent.clientX;
+    const clientXRelativeToGameArea = clientX - gameAreaLeft;
+    const gameAreaMiddleXCoord = gameAreaLeft + gameAreaWidth / 2;
+    let elToCenterPrevElRelativeToWidth;
+    let offsetToCenterElRelativeToOther = 0;
+    if (elToCenterPrevElRelativeTo) {
+      elToCenterPrevElRelativeToWidth = elToCenterPrevElRelativeTo.offsetWidth;
+      offsetToCenterElRelativeToOther =
+        (elToCenterPrevElRelativeToWidth - elWidth) / 2;
+    }
+    let newElLeftRelativeToGameArea;
+    if (
+      clientX > gameAreaLeft + offsetToCenterElRelativeToOther + elWidthHalf
+      && clientX < gameAreaRect.right - elWidthHalf - offsetToCenterElRelativeToOther
+    ) {
+      newElLeftRelativeToGameArea = clientX - gameAreaLeft - elWidthHalf;
+      elStyle.left = numToPx(newElLeftRelativeToGameArea);
+    } else  {
+      if (clientX < gameAreaMiddleXCoord) {
+        newElLeftRelativeToGameArea = offsetToCenterElRelativeToOther;
+      }
+      if (clientX > gameAreaMiddleXCoord) {
+        newElLeftRelativeToGameArea =
+          gameAreaWidth
+          - elWidth
+          - offsetToCenterElRelativeToOther;
+      }
+      elStyle.left = numToPx(newElLeftRelativeToGameArea);
+    }
+    if (el.classList.contains('racket')) {
+      RacketMoving.currRacketLeftRelativeToGameArea = newElLeftRelativeToGameArea;
     }
   }
 }
 
+document.addEventListener('click', async (e) => {
+  const game = new Game();
+  await game.startGame();
+});
+
 class Game {
-  static basePlayerLifes = 3;
-  static currLevelInd = 0;
-  static finalLevelInd = 2;
+  static RACKET_TOP = 250;
+  static statsAndProgressData = {
+    lifes: 3,
+    level: 1,
+    finalLevel: 3,
+    points: 0,
+  };
+  static STAT_OR_PROGRESS_INFO_NAMES = ['level', 'lifes', 'points'];
   static remainingBricksNum = null;
   static isPlaying = false;
   constructor() {
-    Game.currPlayerLifes = Game.basePlayerLifes;
+    reset.init(Game, ['statsAndProgressData'], this);
   }
   async startGame() {
     if (Game.isPlaying) return;
     Game.isPlaying = true;
-    const ballEl = document.querySelector(`.ball`);
-    ballEl.classList.remove('moving-with-racket');
-    const ball = new Ball();
+    const ball = new BallMoving();
     await ball.movePossiblyProcessGameRecursive();
   }
-  setGameAreaChildBasePositions() {
-    const RACKET_TOP = 250;
-    this.centerAndSetBottomCoordGameAreaChild('racket', RACKET_TOP);
-    const ballEl = document.querySelector('.ball');
-    this.centerAndSetBottomCoordGameAreaChild(
-      'ball',
-      RACKET_TOP - ballEl.offsetHeight,
-    );
+  setGameAreaChildBasePositions(childClassesWithCoordsData, isCenter) {
+    childClassesWithCoordsData.forEach((data) => {
+      this.setCoordsGameAreaChild(data, isCenter);
+    });
   }
-  centerAndSetBottomCoordGameAreaChild(className, top) {
+  setCoordsGameAreaChild({ className, top, left }, isCenter) {
     const gameAreaRect = document
       .querySelector('.game-area__inner')
       .getBoundingClientRect();
     const child = document.querySelector(`.${className}`);
     const rect = child.getBoundingClientRect();
     const style = child.style;
-    style.left = numToPx((gameAreaRect.width - rect.width) / 2);
+    if (isCenter) {
+      style.left = numToPx((gameAreaRect.width - rect.width) / 2);
+    } else {
+      style.left = numToPx(left);
+    }
     style.top = numToPx(top);
   }
   fillGameAreaWithBricks() {
@@ -140,33 +162,51 @@ class Game {
     if (bricksGrid.firstElementChild) {
       bricksGrid.innerHTML = '';
     }
-    BRICKS_GRID_POINTS_MATRIXES_GROUPED_BY_LEVELS[Game.currLevelInd].forEach(
-      (row) => row.forEach(
-        (pointsNum) => {
-          bricksGrid.insertAdjacentHTML('beforeend', `
-            <div
-              class="
-                bricks-grid__item
-                ${pointsNum === null ? 'empty-space' : ''}
-                bareer
-              "
-              data-points="${pointsNum}">${pointsNum}
-            </div>
-          `);
-          if (pointsNum) {
-            if (!Game.remainingBricksNum === null) {
-              Game.remainingBricksNum = 0;
-            }
-            Game.remainingBricksNum++;
-          }
+    BRICKS_GRID_POINTS_MATRIXES_GROUPED_BY_LEVELS[
+      Game.statsAndProgressData.level - 1
+    ].forEach((row) => row.forEach((points) => {
+      bricksGrid.insertAdjacentHTML('beforeend', `
+        <div
+          class="
+            bricks-grid__item
+            ${points === null ? 'empty-space' : ''}
+            bareer
+          "
+          data-points="${points}">${points}
+        </div>
+      `);
+      if (points) {
+        if (!Game.remainingBricksNum === null) {
+          Game.remainingBricksNum = 0;
         }
-      )
-    );
+        Game.remainingBricksNum++;
+      }
+    }));
+  }
+  setPlayerStatsAndGameInfo() {
+    const statsEl = document.querySelector('.stats__inner');
+    Game.STAT_OR_PROGRESS_INFO_NAMES.forEach(
+      (name) => {
+        statsEl.insertAdjacentHTML('beforeend', `
+          <li class="stats__item">
+            <span>
+              ${capitalizeFirstLetter(name)}:
+            </span>
+            <span data-stat-or-progress-info-name="${name}">
+              ${Game.statsAndProgressData[name]}
+            </span>
+          </li>
+        `);
+      }
+    )
   }
   processGameAfterClashToBareer(elementsData, edgeNextToBareer) {
     const bareerEl = this.getBareerEl(elementsData, edgeNextToBareer);
     this.possiblyRemoveBrickGetPoints(bareerEl);
-    this.reducePlayerLifesAfterBallClashGameAreaBottom(bareerEl, edgeNextToBareer);
+    this.reducePlayerLifesAfterBallMovingClashGameAreaBottom(
+      bareerEl,
+      edgeNextToBareer,
+    );
   }
   getBareerEl(elementsData, edgeNextToBareer) {
     return elementsData[edgeNextToBareer].find(
@@ -174,45 +214,100 @@ class Game {
     );
   }
   possiblyRemoveBrickGetPoints(bareerEl) {
-    const pointsNum = bareerEl.dataset && bareerEl.dataset.points;
-    if (pointsNum) {
+    const points = +(bareerEl.dataset && bareerEl.dataset.points);
+    if (points) {
       bareerEl.classList.add('empty-space');
       if (Game.remainingBricksNum > 1) {
+        let allPoints = Game.statsAndProgressData.points;
+        allPoints += points;
+        this.findDatasetElUpdateText('points', allPoints);
+        Game.statsAndProgressData.points = allPoints;
         Game.remainingBricksNum--;
       } else {
         Game.remainingBricksNum = null;
-        this.restartGameChangeLevel();
+        this.resetGameDataChangeLevel();
       }
     }
   }
-  reducePlayerLifesAfterBallClashGameAreaBottom(bareerEl, edgeNextToBareer) {
-    if (Game.currPlayerLifes > 1) {
-      Game.currPlayerLifes--;
+  async reducePlayerLifesAfterBallMovingClashGameAreaBottom(
+    bareerEl,
+    edgeNextToBareer,
+  ) {
+    if (
+      !bareerEl.classList.contains('game-area')
+      || edgeNextToBareer !== 'bottom'
+    ) return;
+    const lifesEl = document
+      .querySelector('[data-stat-or-progress-info-name="lifes"]')
+    let lifes = Game.statsAndProgressData.lifes;
+    if (lifes > 1) {
+      lifes--;
+      lifesEl.textContent = lifes;
+      Game.statsAndProgressData.lifes = lifes;
     } else {
-      Game.currPlayerLifes = Game.basePlayerLifes;
+      lifesEl.textContent = 0;
+      Game.reset();
+      lifes = Game.statsAndProgressData.lifes;
+      // for user to see new lifes number before  alert
+      await delay(0);
+      alert('You are lose, restarting game');
+      lifesEl.textContent = lifes;
+      await this.resetGameDataChangeLevel(true);
     }
   }
-  restartGameChangeLevel() {
+  resetGameDataChangeLevel(isLose, isWin) {
     Game.isPlaying = false;
     const ballEl = document.querySelector(`.ball`);
-    ballEl.classList.add('moving-with-racket');
-    this.setGameAreaChildBasePositions();
-    if (Game.currLevelInd === Game.finalLevelInd) {
-      Game.currLevelInd = 0;
-    } else {
-      Game.currLevelInd++;
-    }
+    const racket = document.querySelector(`.racket`);
+    this.setGameAreaChildBasePositions([
+      {
+        className: 'ball',
+        top: Game.RACKET_TOP - ballEl.offsetHeight,
+        left:
+          RacketMoving.currRacketLeftRelativeToGameArea
+          + ((racket.offsetWidth - ballEl.offsetWidth) / 2),
+      },
+    ]);
+    this.changeLevel(isLose);
     this.fillGameAreaWithBricks();
-    this.reset();
+    if (!isLose) return;
+    this.resetStatsOrProgress();
+  }
+  changeLevel(isLose) {
+    let level = Game.statsAndProgressData.level;
+    let isPassedWholeGame;
+    if (level === Game.statsAndProgressData.finalLevel) {
+      alert('You are passed the whole game, play again if you want');
+      level = 1;
+      isPassedWholeGame = true;
+    } else if (!isLose) {
+      level++;
+      this.findDatasetElUpdateText('level', level);
+    }
+    Game.statsAndProgressData.level = level;
+    if (!isPassedWholeGame) return;
+    this.resetStatsOrProgress();
+  }
+  resetStatsOrProgress() {
+    Game.reset();
+    Game.STAT_OR_PROGRESS_INFO_NAMES.forEach((name) => {
+      this.findDatasetElUpdateText(name, Game.statsAndProgressData[name]);
+    });
+  }
+  findDatasetElUpdateText(datasetValue, valueAndFutureText) {
+    const el = document.querySelector(
+      `[data-stat-or-progress-info-name="${datasetValue}"]`,
+    );
+    el.textContent = valueAndFutureText;
   }
 }
 
-class Ball extends Game {
+class BallMoving extends Game {
   constructor() {
     super();
     const BASE_JUMP_OFFSET_DIRECTION_DEG = randomBetween(30, 60);
-    this.BASE_JUMP_OFFSET_DIRECTION_DEG
-      = BASE_JUMP_OFFSET_DIRECTION_DEG;
+    this.BASE_JUMP_OFFSET_DIRECTION_DEG =
+      BASE_JUMP_OFFSET_DIRECTION_DEG;
     // it will be always 2 values for angles after clash because we mirror
     // them to change ball direction completely
     this.NEW_ANGLE_AFTER_CLASH = 180 - 90 - BASE_JUMP_OFFSET_DIRECTION_DEG;
@@ -227,9 +322,9 @@ class Ball extends Game {
         oldLeft: null,
       },
       baseTop: null,
-      isMovingToNotStuckInBareerAtStart: false,
+      isMoving: false,
     };
-    reset.init(Ball, dataToReset, this);
+    reset.init(BallMoving, dataToReset, this);
   }
   async movePossiblyProcessGameRecursive() {
     if (!Game.isPlaying) return;
@@ -241,33 +336,32 @@ class Ball extends Game {
     let left = rect.left - gameAreaRect.left;
     let top = rect.top - gameAreaRect.top;
     let baseTop = this.baseTop;
-    let isMovingToNotStuckInBareerAtStart
-      = this.isMovingToNotStuckInBareerAtStart;
+    let isMoving = this.isMoving;
     let movementData = this.movementData;
     if (!baseTop) {
-      this.lmao = true;
       baseTop = top;
       this.baseTop = baseTop;
-      isMovingToNotStuckInBareerAtStart = true;
-      this.isMovingToNotStuckInBareerAtStart
-        = isMovingToNotStuckInBareerAtStart;
+      isMoving = true;
+      this.isMoving = isMoving;
       movementData.oldTop = top;
       movementData.oldLeft = left;
     }
     // incrementally move ball by >= 2px in Y coordinate after game start
     // to not get stuck in racket first because it's bareer
-    if (top > baseTop - 2 && isMovingToNotStuckInBareerAtStart) {
+    if (top > baseTop - 2 && isMoving) {
       this.move(movementData, left, top);
       await delay(0);
       await this.movePossiblyProcessGameRecursive();
       return;
     }
-    this.isMovingToNotStuckInBareerAtStart = false;
-    const BASE_JUMP_OFFSET_DIRECTION_DEG
-      = this.BASE_JUMP_OFFSET_DIRECTION_DEG;
+    this.isMoving = false;
+    const BASE_JUMP_OFFSET_DIRECTION_DEG =
+      this.BASE_JUMP_OFFSET_DIRECTION_DEG;
     const NEW_ANGLE_AFTER_CLASH = this.NEW_ANGLE_AFTER_CLASH;
     const elementsData = this.getElementsDataFromPointsNextToAllEdges();
-    const edgeNextToBareer = this.getMaxContactWithBareerPointsNumEdge(elementsData);
+    const edgeNextToBareer = this.getMaxContactWithBareerPointsNumEdge(
+      elementsData,
+    );
     if (edgeNextToBareer) {
       let angleNew;
       const { oldTop, oldLeft } = movementData;
@@ -276,53 +370,47 @@ class Ball extends Game {
         case 'top':
           xCoordDirectionDegAntiClockwise = 180;
           if (oldLeft < left) {
-            angleNew
-              = xCoordDirectionDegAntiClockwise
-                - 180
-                + BASE_JUMP_OFFSET_DIRECTION_DEG;
+            angleNew =
+              xCoordDirectionDegAntiClockwise
+              - 180
+              + BASE_JUMP_OFFSET_DIRECTION_DEG;
           } else {
-            angleNew
-              = xCoordDirectionDegAntiClockwise
-                - BASE_JUMP_OFFSET_DIRECTION_DEG;
+            angleNew =
+              xCoordDirectionDegAntiClockwise - BASE_JUMP_OFFSET_DIRECTION_DEG;
           }
           break;
         case 'right':
           xCoordDirectionDegAntiClockwise = 270;
           if (oldTop < top) {
-            angleNew
-              = xCoordDirectionDegAntiClockwise
-                - 180
-                + NEW_ANGLE_AFTER_CLASH;
+            angleNew =
+              xCoordDirectionDegAntiClockwise
+              - 180
+              + NEW_ANGLE_AFTER_CLASH;
           } else {
-            angleNew
-              = xCoordDirectionDegAntiClockwise
-                - NEW_ANGLE_AFTER_CLASH;
+            angleNew =
+              xCoordDirectionDegAntiClockwise - NEW_ANGLE_AFTER_CLASH;
           }
           break;
         case 'bottom':
           xCoordDirectionDegAntiClockwise = 360;
           if (oldLeft > left) {
-            angleNew
-              = xCoordDirectionDegAntiClockwise
-                - 180
-                + BASE_JUMP_OFFSET_DIRECTION_DEG;
+            angleNew =
+              xCoordDirectionDegAntiClockwise
+              - 180
+              + BASE_JUMP_OFFSET_DIRECTION_DEG;
           } else {
-            angleNew
-              = xCoordDirectionDegAntiClockwise
-                - BASE_JUMP_OFFSET_DIRECTION_DEG;
+            angleNew =
+              xCoordDirectionDegAntiClockwise
+              - BASE_JUMP_OFFSET_DIRECTION_DEG;
           }
           break;
         case 'left':
           xCoordDirectionDegAntiClockwise = 90;
           if (oldTop > top) {
-            angleNew
-              = xCoordDirectionDegAntiClockwise
-                - 180
-                + NEW_ANGLE_AFTER_CLASH;
+            angleNew =
+              xCoordDirectionDegAntiClockwise - 180 + NEW_ANGLE_AFTER_CLASH;
           } else {
-            angleNew
-              = xCoordDirectionDegAntiClockwise
-                - NEW_ANGLE_AFTER_CLASH;
+            angleNew = xCoordDirectionDegAntiClockwise - NEW_ANGLE_AFTER_CLASH;
           }
           break;
       }
@@ -419,7 +507,7 @@ class Ball extends Game {
     const style = document.querySelector('.ball').style;
     const angleRadian = movementData.currAngle * Math.PI / 180;
     // broser will update coords with this value as soon as he can, because we using
-    // setTimeout(0) promisification
+    // setTimeout(fun, 0) promisification
     const SPEED_PX = 2;
     style.left = numToPx(left + SPEED_PX * Math.cos(angleRadian));
     style.top = numToPx(top + SPEED_PX * Math.sin(angleRadian));
--- js/mixins/reset.js
+++ js/mixins/reset.js
@@ -1,29 +1,105 @@
 // you should integrate this mixin at the end of class constructor() {}, like this:
-// reset.init(ClassNotClassInstance, dataToReset, this);
+// reset.init(classNotClassInstance, dataToReset, this);
 
 export default {
-  init(ClassNotClassInstance, dataToReset, ClassInstanceContext) {
-    Object.assign(ClassInstanceContext, dataToReset);
-    ClassNotClassInstance.prototype.reset = this.reset.bind(
-      ClassInstanceContext,
+  // pass array as 2nd param if you want to reset static props
+  init(classNotClassInstance, dataToResetMaybeKeys, classInstanceContext) {
+    let dataToReset;
+    let isStaticData;
+    if (Array.isArray(dataToResetMaybeKeys)) {
+      dataToReset = Object.fromEntries(
+        dataToResetMaybeKeys.map((key) => [key, classNotClassInstance[key]]),
+      );
+      isStaticData = true;
+    } else {
+      dataToReset = dataToResetMaybeKeys;
+      Object.assign(classInstanceContext, dataToReset);
+    }
+    classNotClassInstance.reset = this.reset.bind(
+      classNotClassInstance,
       // destroying references to nested object, if we dont do it they are will
       // have irrelevant values
       JSON.parse(JSON.stringify(dataToReset)),
+      classNotClassInstance,
+      classInstanceContext,
+      this,
+      isStaticData
     );
+    classNotClassInstance._resetValue = this._resetValue.bind(
+      classNotClassInstance,
+      JSON.parse(JSON.stringify(dataToReset)),
+      classNotClassInstance,
+      classInstanceContext,
+      this,
+      isStaticData,
+    );
+    this.classNotClassInstance = classNotClassInstance;
   },
-  // carry function, you shouldn't pass args when calling it
-  reset(dataToReset) {
-    Object.keys(dataToReset).forEach((key) => {
-      let value = dataToReset[key];
-      if (_.isObject(value)) {
-        // we need to destroy references to nested objects every time,
-        // or they are will be set after this[key] = dataToReset[key]
-        const objectPossiblyWithNestedObjectWithoutReferencesAfterParse
-          = JSON.parse(JSON.stringify(value));
-        this[key] = objectPossiblyWithNestedObjectWithoutReferencesAfterParse;
-      } else {
-        this[key] = value;
-      }
-    });
+  reset(
+    dataToReset,
+    classNotClassInstance,
+    classInstanceContext,
+    mixinContext,
+    isStaticData,
+    specificKeyOrKeys,
+    specificKeyOrKeysOfThatObjName,
+    isStaticDataAfterInitNotForIt,
+  ) {
+    if (Array.isArray(specificKeyOrKeys)) {
+      specificKeyOrKeys.forEach((key) => {
+        classNotClassInstance._resetValue(
+          key,
+          specificKeyOrKeysOfThatObjName,
+          isStaticDataAfterInitNotForIt,
+        );
+      });
+    } else if (typeof specificKeyOrKeys === 'string') {
+      classNotClassInstance._resetValue(
+        specificKeyOrKeys,
+        specificKeyOrKeysOfThatObjName,
+        isStaticDataAfterInitNotForIt,
+      );
+    } else {
+      Object.keys(dataToReset).forEach((key) => {
+        classNotClassInstance._resetValue(
+          key,
+          specificKeyOrKeysOfThatObjName,
+          isStaticDataAfterInitNotForIt,
+        );
+      });
+    }
+  },
+  _resetValue(
+    dataToReset,
+    classNotClassInstance,
+    classInstanceContext,
+    mixinContext,
+    isStaticData,
+    key,
+    specificKeyOrKeysOfThatObjName,
+    isStaticDataAfterInitNotForIt,
+  ) {
+    const currDataContext =
+      isStaticDataAfterInitNotForIt || isStaticData
+        ? specificKeyOrKeysOfThatObjName
+          ? classNotClassInstance[specificKeyOrKeysOfThatObjName]
+          : classNotClassInstance
+        : specificKeyOrKeysOfThatObjName
+          ? classInstanceContext[specificKeyOrKeysOfThatObjName]
+          : classInstanceContext;
+    const value =
+      specificKeyOrKeysOfThatObjName
+        ? dataToReset[specificKeyOrKeysOfThatObjName][key]
+        : dataToReset[key];
+    if (_.isObject(value)) {
+      // we need to destroy references to nested objects every time,
+      // or they are will be set after this[key] = dataToReset[key]
+      const objectPossiblyWithNestedObjectWithoutReferencesAfterParse =
+        JSON.parse(JSON.stringify(value));
+      currDataContext[key] =
+        objectPossiblyWithNestedObjectWithoutReferencesAfterParse;
+    } else {
+      currDataContext[key] = value;
+    }
   },
 };
